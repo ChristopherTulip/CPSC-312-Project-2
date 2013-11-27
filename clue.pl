483e6dabbb0871271b0577554b038eea432c9168
Chris Tulip <Christopher.tulip@gmail.com>
2013-11-27 09:19:41 -0800

    have the basic repl loop

--- clue.pl
+++ clue.pl
@@ -1,4 +1,5 @@
-%% This is the base file
+%% entry point to the program
+clue_repl :- format("\n\nWelcome to Clue!\nPlease use the following commands:\n\"stop\".\t\t->\tQuit\n\"remove\".\t->\tenter a logical deduction\n"), read(X), process(X), clue_repl.
 
 weapon(dagger).
 weapon(rope).
@@ -24,20 +25,21 @@ room(study).
 room(library).
 room(billiard_room).
 
-list(X)   :- X == rooms, format("test").
-list(X)   :- X == people, format("test").
-list(X)   :- X == weapons, format("test").
-list(X)   :- format("sorry I don't understand what you mean by: ~a\nPlease use the key words people, rooms, or weapons!\n", X).
+process(stop) :- halt.
+process(remove) :- format("Please enter the item that you want to remove\n"), read(X), remove(X).
+process(X) :- format("Sorry I didn't understand your input\n").
 
+remove(stop) :- !.
 remove(X) :- weapon(X), format("removing the following weapon: ~a\n", X), remove_weapon(X).
-remove(X) :- room(X),   remove_room(X).
-remove(X) :- player(X), remove_player(X).
+remove(X) :- room(X), format("removing the following room: ~a\n", X), remove_room(X).
+remove(X) :- player(X),format("removing the following player: ~a\n", X), remove_player(X).
+remove(X) :- format("Sorry I don't understand that item\n"), process(remove).
 
 remove_weapon(X) :- output(weapon).
 remove_room(X)   :- output(room).
 remove_player(X) :- output(player).
 
-output(X) :- format("heck yes ~a", X).
+output(X) :- format("heck yes ~a\n", X).
 
 %% This is how we can output string values
 %% swritef(S, '%w %w blah blah', ['Hello', 'World'])
